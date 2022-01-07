e75a9a39dba60f40fd619f6c3bb964c48df93037
db1692 <donaldbarr22@gmail.com>
2022-01-07 15:24:09 +0000

    Starting a small refresh of the website

---
+++ javascript.js
@@ -0,0 +1,31 @@
+function myFunction() {
+  var dots = document.querySelector(".dots");
+  var moreText = document.querySelector(".more");
+  var btnText = document.querySelector(".myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunction2() {
+  var dots = document.querySelector(".dots2");
+  var moreText = document.querySelector(".more2");
+  var btnText = document.querySelector(".myBtn2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
